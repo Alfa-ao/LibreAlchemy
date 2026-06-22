65ae78ecdb1f4bb15375e4ffec531962832c7e57
Alfa-ao <294923229+Alfa-ao@users.noreply.github.com>
2026-06-22 23:28:23 +0300

    WidgetManager

---
+++ Scripts/UI/AlchemyWidgetManager.lua
@@ -0,0 +1,32 @@
+-- AlchemyWidgetManager.lua
+
+Class( "AlchemyWidgetManager", {
+    _mainForm = nil,
+    _ouText = nil,
+})
+
+function AlchemyWidgetManager:Init()
+    self._mainForm = _G.mainForm
+    self._ouText = self._mainForm:GetChildChecked( "ouText" )
+
+    local pco = common.GetPosConverterParams()
+    local plc = self._ouText:GetPlacementPlain()
+    plc.posX = pco.fullVirtualSizeX / 2 - 360
+    self._ouText:SetPlacementPlain( plc )
+
+    self:InitDragAndDrop()
+    self:Hide()
+end
+
+function AlchemyWidgetManager:InitDragAndDrop()
+    if rawget( _G, "DnD" ) then
+        _G.DnD.Init( self._ouText, nil, true )
+        return true
+    end
+    
+    return false
+end
+
+function AlchemyWidgetManager:Show() self._mainForm:Show( true ) end
+function AlchemyWidgetManager:Hide() self._mainForm:Show( false ) end
+function AlchemyWidgetManager:GetTextWidget() return self._ouText end
