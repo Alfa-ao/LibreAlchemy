69c140a421dacc4c9aa4c95c6fa6a41a376f6e51
Alfa-ao <294923229+Alfa-ao@users.noreply.github.com>
2026-06-22 23:27:05 +0300

    Config c разделением messageType

---
+++ Scripts/Core/AlchemyConfig.lua
@@ -0,0 +1,18 @@
+-- AlchemyConfig.lua
+-- Настройки по умолчанию.
+
+Class( "AlchemyConfig", {
+    MAX_DISPLAY_RESULTS = 5,     -- Максимальное кол-во ТОП-N возможных рецептов
+    DEFAULT_DRUMS = 2,           -- (Re: UIAddon) Кол-во слотов доступных в ступке.
+    DEFAULT_MAX_CORRECTIONS = 6, -- Максимальная коррекция в колбе (в меню варки).
+
+    -- messageType:
+    MESSAGE_GREETINGS = 1,       -- "Приветствую!"
+    MESSAGE_WELCOME_BACK = 2,    -- "С возвращением!"
+    MESSAGE_WARNING = 3,         -- Предупреждение (нет LibDnD)
+    MESSAGE_NORMAL = 0,          -- Остальное для алхимки
+
+    -- Отладка
+    DEBUG = false,
+    DEBUG_REACTION = false,
+})
