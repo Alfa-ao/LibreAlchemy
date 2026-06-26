b363420e3f5d562b325570b3f3573de4d54a41a8
Alfa-ao <294923229+Alfa-ao@users.noreply.github.com>
2026-06-26 16:48:12 +0300

    Добавление логики

--- Scripts/Core/AlchemyInit.lua
+++ Scripts/Core/AlchemyInit.lua
@@ -6,37 +6,47 @@ state.messageType = config.MESSAGE_GREETINGS
 
 -- Сервисы
 local services = { 
-	debug = AlchemyDebugService(),
-	locale = AlchemyLocaleService(), 
-	recipe = AlchemyRecipeService(), 
-	search = AlchemySearchService(), 
+	debug = AlchemyDebugService(),   -- Сервис для отладки (Debug)
+	locale = AlchemyLocaleService(), -- Сервис локализации ENG, RUS
+	recipe = AlchemyRecipeService(), -- Сервис рецепта
 }
 
 services.debug:Init( config )
 services.recipe:Init( state )
-services.search:Init( state, services.recipe )
+
+local mathUtils = MathUtils()       -- Хелперы
+local evaluator = RecipeEvaluator() -- Поиск топового score рецепта
+
+-- Маппер сдвигов
+local drumShiftMapper = DrumShiftMapper()
+drumShiftMapper:Init( state, services.recipe, mathUtils )
+
+-- Алгоритм сервиса search
+local searchAlgorithm = BacktrackingSearchAlgorithm()
+searchAlgorithm:Init( evaluator, mathUtils )
+
+services.search = AlchemySearchService() -- Сервис поиска возможных рецептов
+services.search:Init( state, services.recipe, drumShiftMapper, searchAlgorithm )
 
 -- Интерфейс
 local widgetManager = AlchemyWidgetManager()
 local textFormatter = AlchemyTextFormatter()
-
 textFormatter:Init( widgetManager, services.debug )
 
 -- Обработчики событий
 local systemEvents = AlchemySystemEvents()
-local alchemyEvents = AlchemyEvents()
-local avatarEvents = AlchemyAvatarEvents()
-
+local alchemyEvents = AlchemyEvents()      -- EVENT_ALCHEMY_*
+local avatarEvents = AlchemyAvatarEvents() -- EVENT_AVATAR_*
 systemEvents:Init( state, config, textFormatter, services )
 alchemyEvents:Init( state, config, widgetManager, textFormatter, services )
 avatarEvents:Init( state, config, widgetManager, textFormatter, services )
 
 -- Регистрация событий
-local eventBus = AlchemyEventBus()
-eventBus:Init( systemEvents, alchemyEvents, avatarEvents )
-eventBus:RegisterAll()
+local eventManager = AlchemyEventManager()
+eventManager:Init( systemEvents, alchemyEvents, avatarEvents )
+eventManager:RegisterAll()
 
-----------------------
-if avatar.IsExist() then
-	avatarEvents:OnAvatarCreated( --[[ { id = avatar.GetId() } ]] )
-end
+-- Инициализация
+local bootstrap = AlchemyBootstrap()
+bootstrap:Init( eventManager )
+bootstrap:Run()
