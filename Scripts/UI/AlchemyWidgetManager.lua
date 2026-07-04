c7dcd38c6952da5baa7420ce657756d22b70d885
Alfa-ao <294923229+Alfa-ao@users.noreply.github.com>
2026-07-04 21:50:40 +0300

    Doc: AlchemyWidgetManager (Class)

--- Scripts/UI/AlchemyWidgetManager.lua
+++ Scripts/UI/AlchemyWidgetManager.lua
@@ -1,50 +1,80 @@
--- UI/AlchemyWidgetManager.lua
+--------------------------------------------------------------------------------
+-- Scripts/UI/AlchemyWidgetManager.lua
+-- Менеджер виджетов аддона (AlchemyWidgetManager).
+-- Отвечает за инициализацию, регистрацию и управление жизненным циклом
+-- всех кастомных оберток над нативными виджетами (WidgetClassInterface).
+--------------------------------------------------------------------------------
 
 Class( "AlchemyWidgetManager", {
-    _mainForm = nil,
-    _widgets = {},
+	_mainForm = nil,  -- Ссылка на главную форму аддона (mainForm).
+	_widgets = {},    -- Хеш-таблица зарегистрированных виджетов-оберток { ["widgetName"] = WidgetClassInterface }.
 } )
 
+--------------------------------------------------------------------------------
+-- Инициализация менеджера виджетов.
+-- Принимает произвольное количество экземпляров виджетов, сортирует их по
+-- приоритету инициализации и регистрирует в менеджере.
+--------------------------------------------------------------------------------
 function AlchemyWidgetManager:Init( ... ) --- void
-    self._mainForm = _G.mainForm
-    self._widgets = {}
-    
-    local widgets = { ... }
-    
-    table.sort( widgets, function( widgetA, widgetB )
+	self._mainForm = _G.mainForm
+	self._widgets = {}
+	
+	local widgets = { ... }
+	
+	-- Сортировка виджетов по убыванию приоритета (чем больше число, тем раньше инициализируется)
+	table.sort( widgets, function( widgetA, widgetB )
 		return widgetA:GetPriorityClass() > widgetB:GetPriorityClass()
 	end )
-    
-    for _, widget in ipairs( widgets ) do
-        if widget and InstanceOf( widget, _G.WidgetClassInterface ) then
-            local widgetName = widget:GetWidgetName()
-            self._widgets[widgetName] = widget
-            
-            -- Вызываем внутренний Init виджета, передавая ему ссылку на менеджер
-            if widget.Init then
-                widget:Init( self )
-            end
-        else
-            local objectClass = GetParentClass( widget )
-            local className = GetClassName( objectClass )
-            
-            error( string.format( 
-                "Unsupported class '%s' does not have an interface 'WidgetClassInterface'", 
-                className 
-            ) )
-        end
-    end
+	
+	-- Регистрация и инициализация каждого переданного виджета
+	for _, widget in ipairs( widgets ) do
+		if widget and InstanceOf( widget, _G.WidgetClassInterface ) then
+			local widgetName = widget:GetWidgetName()
+			self._widgets[ widgetName ] = widget
+			
+			-- Вызываем внутренний Init виджета, передавая ему ссылку на менеджер
+			if widget.Init then
+				widget:Init( self )
+			end
+		else
+			-- Ругаемся на соответствие обязательному интерфейсу
+			local objectClass = GetParentClass( widget )
+			local className = GetClassName( objectClass )
+			
+			error( string.format( 
+				"Unsupported class '%s' does not have an interface 'WidgetClassInterface'", 
+				className 
+			) )
+		end
+	end
 end
 
--- Геттер для получения формы (чтобы виджеты не лазили в _mainForm напрямую)
+--------------------------------------------------------------------------------
+-- Получить ссылку на главную форму аддона.
+-- Используется виджетами для получения доступа к нативному дереву элементов
+-- без прямого обращения к глобальной переменной _G.mainForm.
+--------------------------------------------------------------------------------
 function AlchemyWidgetManager:GetMainForm() --- Widget
-    return self._mainForm
+	return self._mainForm
 end
 
--- Геттер конкретного виджета по имени (Выводит контроллер/класс/обвертку над нативным)
+--------------------------------------------------------------------------------
+-- Получить зарегистрированную обертку виджета по его системному имени.
+--------------------------------------------------------------------------------
 function AlchemyWidgetManager:GetWidgetWrapper( widgetName ) --- ?WidgetClassInterface
-    return self._widgets[widgetName] or nil
+	return self._widgets[ widgetName ] or nil
 end
 
-function AlchemyWidgetManager:Show() self._mainForm:Show( true )  end
-function AlchemyWidgetManager:Hide() self._mainForm:Show( false ) end
+--------------------------------------------------------------------------------
+-- Показать главную форму аддона.
+--------------------------------------------------------------------------------
+function AlchemyWidgetManager:Show() --- void
+	self._mainForm:Show( true )
+end
+
+--------------------------------------------------------------------------------
+-- Скрыть главную форму аддона.
+--------------------------------------------------------------------------------
+function AlchemyWidgetManager:Hide() --- void
+	self._mainForm:Show( false )
+end
