20862dded2109479784f90ca90b1e3837431c67c
Alfa-ao <294923229+Alfa-ao@users.noreply.github.com>
2026-07-06 15:44:59 +0300

    Doc + типизация

--- Scripts/UI/Widgets/WidgetOuText.lua
+++ Scripts/UI/Widgets/WidgetOuText.lua
@@ -6,7 +6,8 @@
 Class( "WidgetOuText", WidgetClassInterface() )
 
 --------------------------------------------------------------------------------
--- Инициализация виджета.
+--- Инициализация виджета.
+--- @param widgetManager table - экземпляр класса AlchemyWidgetManager
 --------------------------------------------------------------------------------
 function WidgetOuText:Init( widgetManager ) --- void
     self._widgetManager = widgetManager
@@ -19,25 +20,30 @@ end
 --------------------------------------------------------------------------------
 -- Реализация методов интерфейса WidgetClassInterface.
 --------------------------------------------------------------------------------
-function WidgetOuText:GetPriorityClass() --- int 
+--- @return number
+function WidgetOuText:GetPriorityClass() 
     return 10 
 end
 
-function WidgetOuText:GetNativeWidget() --- ?Widget 
+--- @return userdata | table | nil
+function WidgetOuText:GetNativeWidget()
     return self._widget 
 end
 
-function WidgetOuText:GetWidgetName() --- string 
+--- @return string
+function WidgetOuText:GetWidgetName()
     return "ouText" 
 end
 
 --------------------------------------------------------------------------------
 -- Методы-обертки для работы с содержимым текстового контейнера.
 --------------------------------------------------------------------------------
+--- @param text WString | ValuedText
 function WidgetOuText:PushBackText( text ) --- void
     self._widget:PushBackText( text )
 end
 
+--- @param pos number
 function WidgetOuText:RemoveAt( pos ) --- void
     self._widget:RemoveAt( pos )
 end
@@ -51,10 +57,11 @@ function WidgetOuText:ForceReposition() --- void
 end
 
 --------------------------------------------------------------------------------
--- Возвращает точную пиксельную высоту текущего текстового контента.
--- Использует внутренний __Content.
+--- Возвращает точную пиксельную высоту текущего текстового контента.
+--- Использует внутренний __Content.
+--- @return number
 --------------------------------------------------------------------------------
-function WidgetOuText:GetExactTextHeight() --- number
+function WidgetOuText:GetExactTextHeight()
     self:ForceReposition()
     
     local content = self._widget:GetChildChecked( "__Content", true )
