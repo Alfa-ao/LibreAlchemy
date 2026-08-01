2b46e304d62789c814802fb48c3e4c138317041d
Alfa-ao <294923229+Alfa-ao@users.noreply.github.com>
2026-08-01 21:36:50 +0300

    Create AlchemyTemplateService.lua

---
+++ Scripts/Services/AlchemyTemplateService.lua
@@ -0,0 +1,29 @@
+--------------------------------------------------------------------------------
+-- Services/AlchemyTemplateService.lua
+-- Сервис для работы с локализованными шаблонами аддона.
+--------------------------------------------------------------------------------
+
+Class( "AlchemyTemplateService", {
+	_group = nil,
+} )
+
+--------------------------------------------------------------------------------
+-- Инициализация сервиса шаблонов.
+--------------------------------------------------------------------------------
+function AlchemyTemplateService:Init() --- void
+	self._group = common.GetAddonRelatedTextGroup( "template" )
+end
+
+--------------------------------------------------------------------------------
+--- Получить WString данные по ключу.
+--- @param key string
+--- @return userdata -- ссылка на текстовый ресурс
+--------------------------------------------------------------------------------
+function AlchemyTemplateService:Get( key )
+	if self._group and self._group:HasText( key ) then
+		return self._group:GetText( key )
+	end
+	
+	-- Если ключ не найден, выбрасываем ошибку с указанием отсутствующего ключа
+	error( string.format( "AlchemyTemplateService: Template key '%s' not found in text group.", tostring( key ) ) )
+end
