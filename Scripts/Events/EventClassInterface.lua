7a753a95b26babc3b97caf0de94837b2e105a677
Alfa-ao <294923229+Alfa-ao@users.noreply.github.com>
2026-08-07 21:57:11 +0300

    Update EventClassInterface.lua

--- Scripts/Events/EventClassInterface.lua
+++ Scripts/Events/EventClassInterface.lua
@@ -15,6 +15,15 @@ Class( "EventClassInterface", {
 -- Абстрактные методы (должны быть реализованы в дочерних классах)
 --------------------------------------------------------------------------------
 
+--- @param state table AlchemyState
+--- @param config table AlchemyConfig
+--- @param widgetManager table AlchemyWidgetManager
+--- @param textFormatter table AlchemyTextFormatter
+--- @param services table Services
+function EventClassInterface:Init( state, config, widgetManager, textFormatter, services )
+    error( "EventClassInterface:GetEventMap must be implemented by subclass" )
+end
+
 --- Возвращает таблицу соответствия имен игровых событий методам-обработчикам.
 --- Формат: { [EVENT_NAME] = self.MethodName, ... }
 --- Используется AlchemyEventManager для автоматической регистрации через common.RegisterEventHandler.
