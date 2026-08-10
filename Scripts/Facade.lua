29473c441716866b0886fbf5080cafe8913f908a
Alfa-ao <294923229+Alfa-ao@users.noreply.github.com>
2026-08-10 19:39:58 +0300

    del local

--- Scripts/Facade.lua
+++ Scripts/Facade.lua
@@ -1,7 +1,5 @@
 if rawget( _G, "Facade" ) then
 	Global( "log", _G.Facade.customAO.log ) -- var_dump.
-	
-	local userMods = Facade.AO.userMods
 else
 	Global( "log", function( ... )
 		local args = { ... }
