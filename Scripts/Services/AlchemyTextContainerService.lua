b1834a8ffecd7c4ed95406fe5c02f1cd67e509bf
Alfa-ao <godenergysoulworker@gmail.com>
2026-08-24 22:54:02 +0300

    Update AlchemyTextContainerService.lua

--- Scripts/Services/AlchemyTextContainerService.lua
+++ Scripts/Services/AlchemyTextContainerService.lua
@@ -3,14 +3,23 @@
 -- Сервис для управления текстовым контейнером (TextContainer).
 --------------------------------------------------------------------------------
 
-Class( "AlchemyTextContainerService" )
+Class( "AlchemyTextContainerService", {
+    _wtParent  = nil,
+    _wtTextContainer = nil,
+    _options  = nil,
+} )
 
 --------------------------------------------------------------------------------
 --- Инициализация сервиса.
 --------------------------------------------------------------------------------
-function AlchemyTextContainerService:Init()
-    self._wtPanel = _G.mainForm:GetChildChecked( "Panel" )
-    self._wtOuText = self._wtPanel:GetChildChecked( "ouText" )
+function AlchemyTextContainerService:Init( params )
+    params = type( params ) == "table" and params or {}
+    
+    self._wtParent = self._wtTextContainer:GetParent()
+    
+    self._options = {
+        sizePadding = type( params.sizePadding ) == "number" and params.sizePadding > 1 and params.sizePadding or 15
+    }
 end
 
 --------------------------------------------------------------------------------
@@ -19,15 +28,13 @@ end
 --- @param ... ValuedText | WString | string
 --------------------------------------------------------------------------------
 function AlchemyTextContainerService:SetLines( ... )
-	local linesArray = { ... }
-    
-    self._wtOuText:RemoveItems()
+    self._wtTextContainer:RemoveItems()
 	
-	for _, line in ipairs( linesArray ) do
-		self._wtOuText:PushBackText( line )
+	for _, line in ipairs { ... }  do
+		self._wtTextContainer:PushBackText( line )
 	end
 	
-	-- Выдать высоту текста и подстроить под него сам Panel
+    -- Выдать высоту текста и подстроить под него сам Panel
     local exactHeight = self:GetExactTextHeight()
     self:UpdateSizePanel( exactHeight )
 end
@@ -37,20 +44,22 @@ end
 --- @param textHeight number
 --------------------------------------------------------------------------------
 function AlchemyTextContainerService:UpdateSizePanel( textHeight )
-    local padding = 15 -- Говнокод убрать в конфиг
+    -- Новый отступ от Panel для ouText
+    local ouTextPlc = self._wtTextContainer:GetPlacementPlain()
+    ouTextPlc.posX = self._options.sizePadding
+    ouTextPlc.posY = self._options.sizePadding
+    self._wtTextContainer:SetPlacementPlain( ouTextPlc )
     
-    local ouTextPlc = self._wtOuText:GetPlacementPlain()
-    
-    -- Ширина: отступ слева (posX) + фиксированная ширина текста (sizeX) + отступ справа
-    local targetSizeX = ouTextPlc.posX + ouTextPlc.sizeX + padding
-    -- Высота: отступ сверху (posY) + (fontsize="15") высота текста + отступ снизу
-    local targetSizeY = ouTextPlc.posY + textHeight + padding
+    -- Ширина: отступ слева (posX) и справа + фиксированная ширина текста
+    local targetSizeX = self._options.sizePadding * 2 + ouTextPlc.sizeX
+    -- Высота: отступ сверху (posY) и снизу + высота текста
+    local targetSizeY = self._options.sizePadding * 2 + textHeight
 
     -- Новый размер для Panel
-    local panelPlc = self._wtPanel:GetPlacementPlain()
+    local panelPlc = self._wtParent:GetPlacementPlain()
     panelPlc.sizeX = targetSizeX
     panelPlc.sizeY = targetSizeY
-    self._wtPanel:SetPlacementPlain( panelPlc )
+    self._wtParent:SetPlacementPlain( panelPlc )
 end
 
 --------------------------------------------------------------------------------
@@ -59,9 +68,25 @@ end
 --- @return number
 --------------------------------------------------------------------------------
 function AlchemyTextContainerService:GetExactTextHeight()
-    self._wtOuText:ForceReposition()
+    self._wtTextContainer:ForceReposition()
     
-    local content = self._wtOuText:GetChildChecked( "__Border" ):GetChildChecked( "__Content" )
+    local content = self._wtTextContainer:GetChildChecked( "__Border" ):GetChildChecked( "__Content" )
     local contentPlc = content:GetPlacementPlain()
     return contentPlc.posY + contentPlc.sizeY
+end
+
+--------------------------------------------------------------------------------
+--- Центрирует окно с подсказкой.
+--------------------------------------------------------------------------------
+function AlchemyTextContainerService:UpdateCenterPanel()
+    local pco = common.GetPosConverterParams()
+    local plc = self._wtPanel:GetPlacementPlain()
+
+    -- Центрируем панель по горизонтали
+    plc.posX = pco.fullVirtualSizeX / 2 - CONFIG.GUI.PANEL_HALF_WIDTH - CONFIG.GUI.PANEL_OFFSET_X
+    -- Инвертируем координату Y для корректного отображения относительно верха экрана
+    -- Подробности: https://github.com/Alfa-ao/LibreAlchemyV2/issues/1
+    plc.posY = pco.fullVirtualSizeY - plc.posY -- Переделать потом на основании окна алхимки
+
+    self._wtPanel:SetPlacementPlain( plc )
 end
