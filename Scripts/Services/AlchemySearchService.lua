971ee5c56804c461b201b9aef22d55a3aaf6483b
Alfa-ao <godenergysoulworker@gmail.com>
2026-08-26 23:23:09 +0300

    Update AlchemySearchService.lua

--- Scripts/Services/AlchemySearchService.lua
+++ Scripts/Services/AlchemySearchService.lua
@@ -60,127 +60,31 @@ function AlchemySearchService:FindBestRecipes()
 	self._recipe:FilterByComponents( drumRequiredComponents, totalDrumsCount )
 
 	-- Запускает алгоритм поиска для перебора вариантов с учетом коррекций и доступных линий
-	self._state.foundResults = self._algorithm:Execute( 
-		self._state, 
-		totalCorrections, 
-		linesAvailability 
-	)
-	
---[[
-table(4) {
-    [1] => table(0) {
-        ["components"] => table(0) {
-            [Аспект победителя] => number(1)
-            [Астральность] => number(1)
-            [Биоморфичность] => number(1)
-            [Исцеление] => number(1)
-            [Царственность] => number(1)
-        }
-        ["recipe"] => table(0) {
-            ["componentsCount"] => number(5)
-            ["name"] => WString(32) "Королевское зелье восстановления"
-            ["requiredComponents"] => table(0) {
-                [Аспект победителя] => number(1)
-                [Астральность] => number(1)
-                [Биоморфичность] => number(1)
-                [Исцеление] => number(1)
-                [Царственность] => number(1)
+	self._state.foundResults = self._algorithm:Execute( self._state, totalCorrections, linesAvailability )
+    
+	--log( self._state.foundResults )
+    --[[ 
+    table(1) {
+        [1] => table(2) {
+            ["recipe"] => table(4) {
+                ["componentsCount"] => number(5)
+                ["name"] => WString(19) "Мастеровой кристалл"
+                ["requiredComponents"] => table(2) {
+                    [Астральность] => number(3)
+                    [Царственность] => number(2)
+                }
+                ["score"] => number(116)
             }
-            ["score"] => number(120)
-        }
-        ["shifts"] => table(5) {
-            [1] => number(-0)
-            [2] => number(-1)
-            [3] => number(0)
-            [4] => number(-1)
-            [5] => number(0)
+            ["shifts"] => table(5) {
+                [1] => number(1)
+                [2] => number(0)
+                [3] => number(0)
+                [4] => number(0)
+                [5] => number(-1)
+            }
         }
     }
-    [2] => table(0) {
-        ["components"] => table(0) {
-            [Аспект победителя] => number(1)
-            [Биоморфичность] => number(1)
-            [Исцеление] => number(1)
-            [Призрачность] => number(1)
-            [Технологичность] => number(1)
-        }
-        ["recipe"] => table(0) {
-            ["componentsCount"] => number(5)
-            ["name"] => WString(36) "Кибернетическое зелье восстановления"
-            ["requiredComponents"] => table(0) {
-                [Аспект победителя] => number(1)
-                [Биоморфичность] => number(1)
-                [Исцеление] => number(1)
-                [Призрачность] => number(1)
-                [Технологичность] => number(1)
-            }
-            ["score"] => number(115)
-        }
-        ["shifts"] => table(5) {
-            [1] => number(-0)
-            [2] => number(-1)
-            [3] => number(1)
-            [4] => number(0)
-            [5] => number(0)
-        }
-    }
-    [3] => table(0) {
-        ["components"] => table(0) {
-            [Аспект телохранителя] => number(1)
-            [Биоморфичность] => number(1)
-            [Исцеление] => number(1)
-            [Призрачность] => number(1)
-            [Технологичность] => number(1)
-        }
-        ["recipe"] => table(0) {
-            ["componentsCount"] => number(5)
-            ["name"] => WString(31) "Кибернетическое зелье исцеления"
-            ["requiredComponents"] => table(0) {
-                [Аспект телохранителя] => number(1)
-                [Биоморфичность] => number(1)
-                [Исцеление] => number(1)
-                [Призрачность] => number(1)
-                [Технологичность] => number(1)
-            }
-            ["score"] => number(115)
-        }
-        ["shifts"] => table(5) {
-            [1] => number(3)
-            [2] => number(-1)
-            [3] => number(0)
-            [4] => number(0)
-            [5] => number(0)
-        }
-    }
-    [4] => table(0) {
-        ["components"] => table(0) {
-            [Аспект телохранителя] => number(1)
-            [Астральность] => number(1)
-            [Биоморфичность] => number(1)
-            [Исцеление] => number(1)
-            [Царственность] => number(1)
-        }
-        ["recipe"] => table(0) {
-            ["componentsCount"] => number(5)
-            ["name"] => WString(27) "Королевское зелье исцеления"
-            ["requiredComponents"] => table(0) {
-                [Аспект телохранителя] => number(1)
-                [Астральность] => number(1)
-                [Биоморфичность] => number(1)
-                [Исцеление] => number(1)
-                [Царственность] => number(1)
-            }
-            ["score"] => number(120)
-        }
-        ["shifts"] => table(5) {
-            [1] => number(3)
-            [2] => number(0)
-            [3] => number(0)
-            [4] => number(0)
-            [5] => number(1)
-        }
-    }
-}
-]]
+     ]]
+    
 	return self._state.foundResults
 end
